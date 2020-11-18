efd9eac319257975b22faf315f1eb5ba822a50b8
Merry  HA <69305320+MerryHa@users.noreply.github.com>
2020-11-18 15:47:27 +0900

    JS: Event Delegation

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ const items=document.querySelector('.items');
 const input=document.querySelector('.footer__input');
 const addBtn=document.querySelector('.footer__button');
 
+const section=document.querySelector('section');
+section.addEventListener('click',(e)=>{
+    const target=e.target;
+    switch(target){
+        case "button.footer__button":
+            onAdd();
+            break;
+        case "button.deleteAll":
+
+    }
+});
+
 addBtn.addEventListener('click',()=>{
     onAdd();
 });
@@ -21,7 +33,14 @@ deleteAll.addEventListener('mouseout',()=>{
     bubble.classList.remove('visible');
 });
 
-function onAdd() {
+window.addEventListener('load',()=>{
+    loadItemsFromBrowser();
+});
+window.addEventListener('beforeunload',()=>{
+    saveItemsInBrowser();
+});
+
+function onAdd(){
     const text=input.value;
     if(text===''){
         input.focus();
@@ -32,44 +51,49 @@ function onAdd() {
     input.value='';
     input.focus();
 }
-
+let id =0;
 function createItem(text) {
+    
     const itemRow=document.createElement('li');
     itemRow.setAttribute('class','items__row');
-    
-    const item=document.createElement('div');
-    item.setAttribute('class','item');
-
-    const itemName=document.createElement('span');
-    itemName.setAttribute('class','item__name');
-    itemName.textContent=text;
-
-    const itemDelete=document.createElement('button');
-    itemDelete.setAttribute('class','item__delete');
-    itemDelete.innerHTML='<i class="fas fa-trash-alt"></i>';
-    itemDelete.addEventListener('click',(e)=>{
-        items.removeChild(itemRow);
-        //itemRow.remove();
-    });
-    deleteAll.addEventListener('click',()=>{
-        if(confirm("Are you sure you want to delete all of them?")){
-        items.innerHTML='';
-        return;
-        }else{
-        return;}
-    });
-
-    const itemDivider=document.createElement('div');
-    itemDivider.setAttribute('class','item__divider');
-
+    itemRow.setAttribute('data-id',id);
+    itemRow.innerHTML=`
+        <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="fas fa-trash-alt" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item__divider"></div>`;
+    id++;
     items.append(itemRow);
-    itemRow.append(item);
-    itemRow.append(itemDivider);
-    item.append(itemName);
-    item.append(itemDelete);   
-
-    
-    item.scrollIntoView({block:'end'});
+    itemRow.scrollIntoView({block:'end'});
+   
+} 
+deleteAll.addEventListener('click',()=>{
+    if(confirm("Are you sure you want to delete all of them?")){
+    items.innerHTML='';
     return;
-}
+    }else{
+    return;}
+});
+
+items.addEventListener('click',event=>{
+    const id=event.target.dataset.id;
+    if(id){
+         const toBeDeleted=document.querySelector(`.items__row[data-id="${id}"]`);
+         toBeDeleted.remove();
+    }
+   
+});
     
+function saveItemsInBrowser() {
+        localStorage.setItem("items",items.innerHTML);
+        localStorage.setItem("itemID",id);
+    }
+function loadItemsFromBrowser(){
+        const loadedItems=localStorage.getItem("items");
+        const loadedID=localStorage.getItem("itemID");
+        items.innerHTML=loadedItems;
+        id=loadedID;
+    }
